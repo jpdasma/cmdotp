68b30a63862223b73451bb81df104db8b1fb2d7b
Julian Dasmarinas <julian.dasma@gmail.com>
2018-04-04 14:03:53 +0800

    decided to use cryptography module as backend

---
+++ cmdotp/storage.py
@@ -0,0 +1,51 @@
+# -*- coding: utf-8 -*-
+# Copyright (C) 2018 Julian Paul Dasmariñas
+# 
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+import os
+import json
+from cryptography.hazmat.primitives.ciphers import Cipher, algorithms, modes
+from from cryptography.hazmat.backends import default_backend
+
+class Storage(object):
+    """
+    """
+
+    def __init__(self, *args, **kwargs):
+        pass
+
+    def write(self):
+        """
+        """
+        pass
+
+    def read(self):
+        """
+        """
+        pass
+
+    def _generate_key(self):
+        pass
+
+    def _encrypt(self):
+        pass
+
+    def _decrypt(self):
+        pass
+
+    def _json_to_binary(self):
+        pass
+
+    def _binary_to_json(self):
+        pass
